76c50cda12571909f50b73355f87d82a9b54f9e1
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-06 17:21:30 +0700

    buat composable TampilData untuk menampilkan detail pengguna

--- app/src/main/java/com/example/tugas6/MainActivity.kt
+++ app/src/main/java/com/example/tugas6/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.navigasi.DataApp
 import com.example.tugas6.ui.theme.Tugas6Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Tugas6Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DataApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
