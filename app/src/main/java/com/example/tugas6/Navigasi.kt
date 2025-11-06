4dc22cf0ecf01fdb852b13c5e1d7f5cb81e51980
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-06 17:26:43 +0700

    tambahkan Scaffold dengan NavHost untuk navigasi halaman

--- app/src/main/java/com/example/tugas6/Navigasi.kt
+++ app/src/main/java/com/example/tugas6/Navigasi.kt
@@ -23,3 +23,10 @@ fun DataApp(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier
 ){
+    Scaffold { isiRuang->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+
+    }
