2670a05050d1b4111178054d03d2fffe34851d73
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-06 15:44:51 +0700

    menambahkan komponen Column untuk menata konten pada halaman utama

--- app/src/main/java/com/example/tugas6/view/Formulir.kt
+++ app/src/main/java/com/example/tugas6/view/Formulir.kt
@@ -1,13 +1,18 @@
 package com.example.tugas6.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.tugas6.R
@@ -27,5 +32,13 @@ fun FormIsian(
 
             )
         }
-    ){}
+    ){ isiRuang ->
+        Column (modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+
+        }
+
+    }
 }
