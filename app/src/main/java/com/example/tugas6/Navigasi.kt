329b88d3a2bfbd50061e94340c9d13c88380323f
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-06 17:26:03 +0700

    buat composable DataApp dengan navigasi antara FormIsian dan TampilData

---
+++ app/src/main/java/com/example/tugas6/Navigasi.kt
@@ -0,0 +1,25 @@
+ackage com.example.navigasi
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import com.example.tugas6.view.FormIsian
+import com.example.tugas6.view.TampilData
+import com.example.tugas6.view.TampilData
+
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+){
