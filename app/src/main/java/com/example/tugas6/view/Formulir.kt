f433a602d389c0fabd682ae9065fdd7ad86ee7fc
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-06 15:42:24 +0700

    menambahkan struktur Scaffold dengan TopAppBar pada halaman utama

--- app/src/main/java/com/example/tugas6/view/Formulir.kt
+++ app/src/main/java/com/example/tugas6/view/Formulir.kt
@@ -1,11 +1,31 @@
 package com.example.tugas6.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.tugas6.R
 
 @Composable
 fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki","Perempuan"),
     OnSumbitBtnClick : () -> Unit
 ){
+    Scaffold (modifier=Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home),
+                    color = Color.White)},
+                Colors = TopAppBarDefaults.topAppBarColors
+                    (containerColor = colorResource(id = R.color.teal_700))
 
+            )
+        }
+    ){}
 }
