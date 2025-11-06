5024131dbd2e40c26dd7d25d244ab93fe8e61b34
Ekoramadhan13 <enugraha411@gmail.com>
2025-11-06 15:35:04 +0700

    menambahkan komponen FormIsian untuk input jenis kelamin dan tombol submit

---
+++ app/src/main/java/com/example/tugas6/view/Formulir.kt
@@ -0,0 +1,11 @@
+package com.example.tugas6.view
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki","Perempuan"),
+    OnSumbitBtnClick : () -> Unit
+){
+
+}
